1dafeddfb283c470a8aef8150a9655a41344f5ad
Kevin T <30321316+glaciyan@users.noreply.github.com>
2023-03-04 09:21:20 +0100

    fix selectors

--- src/crawler/selectors.ts
+++ src/crawler/selectors.ts
@@ -1,30 +1,30 @@
 export const CharacterSelectors = {
-    Name: "body > div > div.wp-block-columns > div:nth-child(2) > div.entry-content.wp-block-post-content > table.genshin_table.main_table > tbody > tr:nth-child(1) > td:nth-child(3)",
+    Name: "table.genshin_table.main_table > tbody > tr:nth-child(1) > td:nth-child(3)",
     Element:
-        "body > div > div.wp-block-columns > div:nth-child(2) > div.entry-content.wp-block-post-content > table.genshin_table.main_table > tbody > tr:nth-child(7) > td:nth-child(2)",
-    Weapon: "body > div > div.wp-block-columns > div:nth-child(2) > div.entry-content.wp-block-post-content > table.genshin_table.main_table > tbody > tr:nth-child(6) > td:nth-child(2)",
+        "table.genshin_table.main_table > tbody > tr:nth-child(7) > td:nth-child(2)",
+    Weapon: "table.genshin_table.main_table > tbody > tr:nth-child(6) > td:nth-child(2)",
     RarityStars:
-        "body > div > div.wp-block-columns > div:nth-child(2) > div.entry-content.wp-block-post-content > table.genshin_table.main_table > tbody > tr:nth-child(5) > td:nth-child(2) > img",
+        "table.genshin_table.main_table > tbody > tr:nth-child(5) > td:nth-child(2) > img",
     Sub: "#char_stats > div:nth-child(2) > table > thead > tr > td:nth-child(7)",
     Constellation:
-        "body > div > div.wp-block-columns > div:nth-child(2) > div.entry-content.wp-block-post-content > table.genshin_table.main_table > tbody > tr:nth-child(11) > td:nth-child(2)",
+        "table.genshin_table.main_table > tbody > tr:nth-child(11) > td:nth-child(2)",
     Description:
-        "body > div > div.wp-block-columns > div:nth-child(2) > div.entry-content.wp-block-post-content > table.genshin_table.main_table > tbody > tr:nth-child(16) > td:nth-child(2)",
+        "table.genshin_table.main_table > tbody > tr:nth-child(16) > td:nth-child(2)",
 };
 
 export const CharacterItemSelectors = {
-    Boss: "body > div > div.wp-block-columns > div:nth-child(2) > div.entry-content.wp-block-post-content > table.genshin_table.main_table > tbody > tr:nth-child(17) > td:nth-child(2) > a:nth-child(5) > div > img",
-    Local: "body > div > div.wp-block-columns > div:nth-child(2) > div.entry-content.wp-block-post-content > table.genshin_table.main_table > tbody > tr:nth-child(17) > td:nth-child(2) > a:nth-child(6) > div > img",
-    Weekly: "body > div > div.wp-block-columns > div:nth-child(2) > div.entry-content.wp-block-post-content > table.genshin_table.main_table > tbody > tr:nth-child(18) > td:nth-child(2) > a:nth-child(4) > div > img",
+    Boss: "table.genshin_table.main_table > tbody > tr:nth-child(17) > td:nth-child(2) > a:nth-child(5) > div > img",
+    Local: "table.genshin_table.main_table > tbody > tr:nth-child(17) > td:nth-child(2) > a:nth-child(6) > div > img",
+    Weekly: "table.genshin_table.main_table > tbody > tr:nth-child(18) > td:nth-child(2) > a:nth-child(4) > div > img",
     FirstCommon:
-        "body > div > div.wp-block-columns > div:nth-child(2) > div.entry-content.wp-block-post-content > table.genshin_table.main_table > tbody > tr:nth-child(17) > td:nth-child(2) > a:nth-child(7) > div > img",
+        "table.genshin_table.main_table > tbody > tr:nth-child(17) > td:nth-child(2) > a:nth-child(7) > div > img",
     FirstBook:
-        "body > div > div.wp-block-columns > div:nth-child(2) > div.entry-content.wp-block-post-content > table.genshin_table.main_table > tbody > tr:nth-child(18) > td:nth-child(2) > a:nth-child(1) > div > img",
+        "table.genshin_table.main_table > tbody > tr:nth-child(18) > td:nth-child(2) > a:nth-child(1) > div > img",
 };
 
 export const ItemSelectors = {
-    Name: "body > div > div.wp-block-columns > div:nth-child(2) > div.entry-content.wp-block-post-content > table.genshin_table.main_table > tbody > tr:nth-child(1) > td:nth-child(3)",
+    Name: "table.genshin_table.main_table > tbody > tr:nth-child(1) > td:nth-child(3)",
     RarityStars:
-        "body > div > div.wp-block-columns > div:nth-child(2) > div.entry-content.wp-block-post-content > table.genshin_table.main_table > tbody > tr:nth-child(4) > td:nth-child(2) > img",
-    Image: "body > div > div.wp-block-columns > div:nth-child(2) > div.entry-content.wp-block-post-content > table.genshin_table.main_table > tbody > tr:nth-child(1) > td.rar_bg_5.hmb > img",
+        "table.genshin_table.main_table > tbody > tr:nth-child(4) > td:nth-child(2) > img",
+    Image: "table.genshin_table.main_table > tbody > tr:nth-child(1) > td.rar_bg_5.hmb > img",
 };
